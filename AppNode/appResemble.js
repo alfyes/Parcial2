9ba37abd79f68a2b0cbffca04aafb27ccd797208
alfyes <alfyes@hotmail.com>
2018-05-14 00:25:31 -0500

    feat: Se agrega apliación en noje js para la ejcución de resemble.

---
+++ AppNode/appResemble.js
@@ -0,0 +1,46 @@
+const compareImages = require('resemblejs/compareImages');
+const fs = require("mz/fs");
+
+async function getDiff(imgIni, imgFinal, resultado){
+
+    process.argv.forEach(function (val, index, array) {
+        console.log(index + ': ' + val);
+    });
+
+    const options = {
+        output: {
+            errorColor: {
+                red: 255,
+                green: 0,
+                blue: 255
+            },
+            errorType: 'movement',
+            transparency: 1,
+            largeImageThreshold: 1200,
+            useCrossOrigin: false,
+            outputDiff: true
+        },
+        scaleToSameSize: true,
+        ignore: ['less'],
+    };
+    try {
+        const data = await
+        compareImages(
+            await fs.readFile(imgIni),
+            await fs.readFile(imgFinal),
+            options
+        );
+
+        console.log(data);
+
+        await fs.writeFile(resultado, data.getBuffer());
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+getDiff(process.argv[2],
+        process.argv[3],
+        process.argv[4]);
+
